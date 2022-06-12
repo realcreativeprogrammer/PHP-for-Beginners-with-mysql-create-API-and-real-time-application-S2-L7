e491de1275c95daa99cb03b9259c09451873148c
realcreativeprogrammer <realcreativeprogrammer@gmail.com>
2022-06-12 11:42:17 +0300

    first

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+$x=10;
+$y=1;
+$str='1';
+
+var_export($x == $y);
+echo '<br>';
+var_export(4 === '4');
+echo '<br>';
+var_export('4' === '4');
+
+echo '<br>';
+var_export(4 != 3);
+echo '<br>';
+var_export(4 > 33);
+echo '<br>';
+var_export(4 > 4);
+echo '<br>';
+var_export(4 >= 4);
+echo '<br>';
+var_export(4 !== 32);
+?>
